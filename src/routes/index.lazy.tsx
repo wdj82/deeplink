4e89ad03c72705333c822d5ea027fb7c456ed30a
William <wjones@clearc2.com>
2025-01-08 10:34:28 -0600

    add alert

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -4,6 +4,18 @@ export const Route = createLazyFileRoute('/')({
     component: Index,
 });
 
+const getMobileOS = () => {
+    const ua = navigator.userAgent;
+    if (/android/i.test(ua)) {
+        return 'Android';
+    } else if (/iPad|iPhone|iPod/.test(ua)) {
+        return 'iOS';
+    }
+    return 'Other';
+};
+
+alert(getMobileOS());
+
 function Index() {
     return (
         <div className='p-2'>
