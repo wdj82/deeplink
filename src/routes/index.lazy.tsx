22703ca37b036cda79390723464a3f0da64bd558
William <wjones@clearc2.com>
2025-02-11 11:58:10 -0600

    add test inputs

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -19,13 +19,15 @@ function Index() {
     const currentOS = getMobileOS();
     if (currentOS !== 'Other') {
         return (
-            <div>
+            <div style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
                 <div>On Mobile!</div>
                 <a href='c2mobileapp://?openDocument=true&class=C2Ticket&id=B723B007C515C337E496827A82D12A34'>
                     Open mobile app!
                 </a>
                 <a href='itms-apps://itunes.apple.com/us/app/c2crm/id605389749'>Download iOS</a>
                 <a href='https://play.google.com/store/apps/details?id=com.clearc2.C2CRM'>Download Android</a>
+                <input type='date' />
+                <input type='time' />
             </div>
         );
     }
